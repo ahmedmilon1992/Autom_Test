fb1a6a37b82417968b36077f05ce507be97c129d
ahmed <ahmedmilon1992@gmail.com>
2023-10-17 20:38:07 -0400

    Initial commit

---
+++ src/D/SDD.java
@@ -0,0 +1,20 @@
+package D;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SDD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 ChromeDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
